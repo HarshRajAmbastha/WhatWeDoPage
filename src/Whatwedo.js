1cbb29129bab2b09527f53336190bc82d816db30
HarshRajAmbastha <harsh_ra@ce.iitr.ac.in>
2021-09-04 00:53:31 +0530

    button and card body to be adjusted

--- src/Whatwedo.js
+++ src/Whatwedo.js
@@ -13,7 +13,7 @@ const Whatwedo = () => {
 
 
 {/* starts here */}
-      <div class="row row-cols-1 row-cols-sm-2 row-cols-md-3 g-3">
+      <div class="row row-cols-1 row-cols-sm-2 row-cols-md-4 g-4">
         <div class="col">
           <div class="card shadow-sm">
             <svg class="bd-placeholder-img card-img-top" width="10%" height="225" xmlns="http://www.w3.org/2000/svg" role="img" aria-label="Placeholder: Thumbnail" preserveAspectRatio="xMidYMid slice" focusable="false"><title>Placeholder</title><rect width="100%" height="100%" fill="#55595c"></rect><text x="50%" y="50%" fill="#eceeef" dy=".3em">
@@ -24,7 +24,7 @@ const Whatwedo = () => {
               {/* <p class="card-text" className="wwd-card-text wwd-p">Project Name */}
               {/* <button type="button"  className="wwd-button"> </button> */}
               {/* </p>   */}
-              
+              <button type="button" class="btn btn-primary btn-circle btn-sm">+</button>
              
               <div class="d-flex justify-content-between align-items-center">
                 <div class="btn-group">
@@ -121,6 +121,42 @@ const Whatwedo = () => {
         </div>
 
         
+
+        <div class="col">
+          <div class="card shadow-sm">
+            <svg class="bd-placeholder-img card-img-top" width="100%" height="225" xmlns="http://www.w3.org/2000/svg" role="img" aria-label="Placeholder: Thumbnail" preserveAspectRatio="xMidYMid slice" focusable="false"><title>Placeholder</title><rect width="100%" height="100%" fill="#55595c"></rect><text x="50%" y="50%" fill="#eceeef" dy=".3em">    </text></svg>
+
+            <div class="card-body">
+              {/* <p class="card-text" className="wwd-card-text wwd-p">Project Name<button type="button"  className="wwd-button">+</button></p> */}
+              <div class="d-flex justify-content-between align-items-center">
+                <div class="btn-group">
+        
+                 
+                </div>
+               
+              </div>
+            </div>
+          </div>
+        </div>
+
+
+        <div class="col">
+          <div class="card shadow-sm">
+            <svg class="bd-placeholder-img card-img-top" width="100%" height="225" xmlns="http://www.w3.org/2000/svg" role="img" aria-label="Placeholder: Thumbnail" preserveAspectRatio="xMidYMid slice" focusable="false"><title>Placeholder</title><rect width="100%" height="100%" fill="#55595c"></rect><text x="50%" y="50%" fill="#eceeef" dy=".3em">    </text></svg>
+
+            <div class="card-body">
+              {/* <p class="card-text" className="wwd-card-text wwd-p">Project Name<button type="button"  className="wwd-button">+</button></p> */}
+              <div class="d-flex justify-content-between align-items-center">
+                <div class="btn-group">
+        
+                 
+                </div>
+               
+              </div>
+            </div>
+          </div>
+        </div>
+
       </div>
     </div>
             
